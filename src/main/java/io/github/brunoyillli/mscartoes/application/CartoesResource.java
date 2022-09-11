70d8a4beb74c62e479da14295807e058b4487e92
Bruno Mendes <wolfteam1910@gmail.com>
2022-09-11 20:01:07 -0300

    salvando cartao e começando busca de limite

--- src/main/java/io/github/brunoyillli/mscartoes/application/CartoesResource.java
+++ src/main/java/io/github/brunoyillli/mscartoes/application/CartoesResource.java
@@ -1,15 +1,34 @@
 package io.github.brunoyillli.mscartoes.application;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.brunoyillli.mscartoes.application.representation.CartaoSaveRequest;
+import io.github.brunoyillli.mscartoes.domain.Cartao;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("cartoes")
+@RequiredArgsConstructor
 public class CartoesResource {
-
+	
+	private final CartaoService service;
+	
 	@GetMapping
 	public String status() {
 		return "ok";
+	}
+	
+	@PostMapping
+	public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request ) {
+		Cartao cartao = request.toModel();
+		service.save(cartao);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }
