6c71b7b022ff631d0bc7956e8b29a4b5d44913d1
Bruno Mendes <wolfteam1910@gmail.com>
2022-09-11 20:17:04 -0300

    terminando o filtro para pegar cartoes com renda maior que o limite

--- src/main/java/io/github/brunoyillli/mscartoes/application/CartoesResource.java
+++ src/main/java/io/github/brunoyillli/mscartoes/application/CartoesResource.java
@@ -1,11 +1,16 @@
 package io.github.brunoyillli.mscartoes.application;
 
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.brunoyillli.mscartoes.application.representation.CartaoSaveRequest;
@@ -30,5 +35,11 @@ public class CartoesResource {
 		Cartao cartao = request.toModel();
 		service.save(cartao);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@GetMapping(params = "renda")
+	public ResponseEntity<List<Cartao>> getCartoesRendaAteh(@RequestParam("renda") Long renda){
+		List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+		return ResponseEntity.ok(list);
 	}
 }
